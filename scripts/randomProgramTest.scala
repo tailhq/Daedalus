58f507261e445f259616541f36f03be3e35ee731
mandar2812 <mandar2812@gmail.com>
2016-10-10 18:22:15 +0200

    Added Dirichlet prior based Generative Turing Model

--- scripts/randomProgramTest.scala
+++ scripts/randomProgramTest.scala
@@ -1,10 +1,9 @@
 import breeze.linalg.DenseVector
-import breeze.stats.distributions.Dirichlet
-import io.github.mandar2812.daedalus.utils._
+import breeze.stats.distributions.{Dirichlet, Multinomial}
 import io.github.mandar2812.daedalus.epistemics._
 import io.github.mandar2812.dynaml.probability.RandomVariable
 import io.github.mandar2812.dynaml.analysis.VectorField
-import breeze.stats.distributions.Dirichlet
+import breeze.stats.distributions._
 
 import scala.util.Random
 
@@ -49,3 +48,33 @@ val res: Seq[Option[String]] = (1 to 10000).map(_ => progRV.sample()).map(p => {
     println("\n")
   }
 }).toSeq.filterNot(c => c == None || c == Some(">01010101"))
+
+
+val stPrior = MeasurableFunction(RandomVariable(new Poisson(4.5)))(DataPipe((x: Int) => x+2))
+
+val dirModel = new DirichletTuringModel(stPrior)
+
+
+val res: Seq[(Int, Option[String])] = (1 to 50).map(_ => {
+  val s = dirModel.sample()
+
+  (1 to 2000).map(_ => {
+    (s._1, s._2.sample())
+  }).toSeq
+
+}).flatten.map(p => {
+  val tape1 = "00000101".finiteTape
+  println("Tape Before: ")
+  println(tape1)
+  try {
+    tape1(p._2)
+    println("Result")
+    println(tape1)
+    (p._1, Some(tape1.toString))
+  } catch {
+    case e: StackOverflowError => (p._1, None)
+    case e: Exception => (p._1, None)
+  } finally {
+    println("\n")
+  }
+}).toSeq.filterNot(c => c._2 == None)
---
+++ src/main/scala-2.11/io/github/mandar2812/daedalus/epistemics/DirichletTuringModel.scala
@@ -0,0 +1,31 @@
+package io.github.mandar2812.daedalus.epistemics
+
+import breeze.linalg.DenseVector
+import breeze.stats.distributions.Dirichlet
+import io.github.mandar2812.dynaml.analysis.VectorField
+import io.github.mandar2812.dynaml.pipes.DataPipe
+import io.github.mandar2812.dynaml.probability.{ProbabilityModel, RandomVariable}
+
+import scala.util.Random
+
+/**
+  * Created by mandar on 10/10/2016.
+  */
+class DirichletTuringModel(priorNumStates: RandomVariable[Int], cellStates: Int = 2)
+  extends EpistemicTuringModel[Int](
+    priorNumStates,
+    DataPipe((  states) => {
+      implicit val ev = VectorField(3*cellStates*states)
+      val baseVec = () => DenseVector.tabulate[Double](3*cellStates*states)(i => math.abs(2.0*Random.nextGaussian()))
+      val dirichlet = RandomVariable(new Dirichlet(baseVec()))
+      val keys = for(state <- 0 until states; cell <- 0 until cellStates) yield (state, cell)
+      val stateProbMap = keys.map(i => {
+        (i, dirichlet.sample())
+      }).toMap
+
+      val cond = (c: (Int, Int)) => {
+        stateProbMap(c)
+      }
+
+      new MarkovTuringProcess(states, cond)
+  }))
